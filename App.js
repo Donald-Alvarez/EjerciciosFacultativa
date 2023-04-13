094cec7942e4fdc880351ba7e02a72d3c031c690
Donald-Alvarez <donaldoctavio0210@gmail.com>
2023-04-13 08:45:44 -0600

    Actualizacion

--- App.js
+++ App.js
@@ -27,3 +27,17 @@ var f= (x, j,a)=>{
 console.log(f(10,10,5));
 
 
+//Ejercicio 25.
+var Resultado = (x,f,m) =>{
+    console.log((x+f)*m);
+}
+console.log('El resultado es:')
+console.log(Resultado(10,10,2));
+
+//Ejercicio 26.
+
+var Resultado = (x,f,m,j) =>{
+    console.log((x-f)*(m/j));
+}
+console.log('El resultado es:')
+console.log(Resultado(10,5,4,2));
--- MyS.js
+++ MyS.js
@@ -1,15 +1,30 @@
-//Ejercicio 25.
-var Resultado = (x,f,m) =>{
-    console.log((x+f)*m);
+//Ejercicio 10
+function positivo(num) {
+    return num >= 0;
+}
+console.log(positivo(10));
+
+//Ejercicio 25
+function suma (a,b,j){
+    return suma = (a+b)*j;
 }
 console.log('El resultado es:')
-console.log(Resultado(10,10,2));
+console.log(suma(10,20,2));
 
-//Ejercicio 26.
-
-var Resultado = (x,f,m,j) =>{
-    console.log((x-f)*(m/j));
+//Ejercicio 26
+function resultado (x,f,j,m){
+    return resultado = ((x-f)*(m/j));
 }
 console.log('El resultado es:')
-console.log(Resultado(10,5,4,2));
+console.log(resultado(10,5,4,2));
 
+//Ejercicio 29
+
+function calculos (dato1, dato2, booleano){
+    if (booleano){
+        return Math.pow(dato1, dato2);
+    }else {
+        return Math.sqrt(dato2);
+    }
+}
+console.log(calculos(5, 3, true))
